7c6fa2feafa26d741ddb5e781381bd11208ac6a8
bobbino <sasso.roberto@gmail.com>
2015-01-07 19:56:59 +0100

    fist steps
    just a plain, weird copy and paste of crappy code. need to change it all

---
+++ patternmatch.hs
@@ -0,0 +1,31 @@
+import Data.Char
+
+--throw an expected error
+expected s = s ++ " expected"
+
+--Tests two chars match
+matchChar :: Char -> Char -> Bool
+matchChar x y = x == y
+
+--Gets an identifier
+--Throws an expected error if it is not an alpha 
+getName :: Char -> Char
+getName x
+	| isAlpha x = x
+	| otherwise = error (expected "Name")
+
+--Checks if the char is a digit
+--Throws an expected error if it is not a digit
+getNum :: Char -> Char
+getNum x
+	| isDigit x = x
+	| otherwise = error (expected "Integer")
+
+--Prefix a string with a tab
+emit s = "\t" ++ s
+
+--Prefix a string with a tab and postfix it with a new line
+emitLn s = (emit s) ++ "\n"
+
+expression x = emitLn ("MOVE" ++ [num] ++ ",DO")
+	where num = getNum x
